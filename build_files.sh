568152e10748a254756fb9c111e0246cce067663
govardhan <115453040+Govardhan62@users.noreply.github.com>
2024-09-26 22:30:43 +0530

    added build_files

---
+++ build_files.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Ensure the Python environment is set up correctly
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+# Apply database migrations
+python manage.py migrate --noinput
+# Collect static files
+python manage.py collectstatic --noinput
