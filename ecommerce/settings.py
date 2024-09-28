46253882509fb6c0083606fb6b4f5491bd970098
govardhan <115453040+Govardhan62@users.noreply.github.com>
2024-09-28 10:22:36 +0530

    modified:   ecommerce/settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import dj_database_url
+import dj_database_url # type: ignore
 from pathlib import Path
 import os
 
@@ -21,11 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v13oa1$62g-r9+%zlk3u4285kq%^@3^$7-q@834c3i0!x+6u@)'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ['*']
+DEBUG = os.environ.get("DEBUG","False").lower() =="true"
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -89,7 +90,8 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] =dj_database_url.parse("postgresql://pretty_django_rq0z_user:rCAfWSNfchPGTOfE0yYE5joBCrcRtsOi@dpg-crqnv8bv2p9s73e79v2g-a.oregon-postgres.render.com/Docutrakr")
+database_url =os.environ.get("DATABASE_URL")
+DATABASES['default'] =dj_database_url.parse(database_url)
 
 
 # Password validation
